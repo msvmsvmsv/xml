736607ad25819e6a09dcc2ad581ac96bb96c941c
msokol <msokol@by.byelex.com>
2021-01-29 17:43:51 +0300

    Initial commit

---
+++ src/com/company/BasicXsl.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class BasicXsl {
+    // This method applies the xslFilename to inFilename and writes
+    // the output to outFilename.
+    public static void xsl(String inFilename, String outFilename, String xslFilename) {
+        try {
+            // Create transformer factory
+            TransformerFactory factory = TransformerFactory.newInstance();
+
+            // Use the factory to create a template containing the xsl file
+            Templates template = factory.newTemplates(new StreamSource(
+                    new FileInputStream(xslFilename)));
+
+            // Use the template to create a transformer
+            Transformer xformer = template.newTransformer();
+
+            // Prepare the input and output files
+            Source source = new StreamSource(new FileInputStream(inFilename));
+            Result result = new StreamResult(new FileOutputStream(outFilename));
+
+            // Apply the xsl file to the source file and write the result
+            // to the output file
+            xformer.transform(source, result);
+        } catch (FileNotFoundException e) {
+        } catch (TransformerConfigurationException e) {
+            // An error occurred in the XSL file
+        } catch (TransformerException e) {
+            // An error occurred while applying the XSL file
+            // Get location of error in input file
+            SourceLocator locator = e.getLocator();
+            int col = locator.getColumnNumber();
+            int line = locator.getLineNumber();
+            String publicId = locator.getPublicId();
+            String systemId = locator.getSystemId();
+        }
+    }
+
+    public static void main(String[] args) {
+        BasicXsl.xsl("in.xml", "out.xml","xsl.xml");
+    }
+}
